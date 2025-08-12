7573b311ddc157516431606998e286ccc2f2120f
Bulycheva Darya <evil.lama@yandex.ru>
2025-08-12 21:38:05 +0300

    debug

--- smoke.sh
+++ smoke.sh
@@ -4,7 +4,7 @@ set -e
 
 sleep 10
 
-status_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/posts)
+status_code=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/posts)
 
 if [[ "$status_code" -eq 200 ]]; then
     exit 0
